bd10ddfd9a6aafc21af44d3ef26a86287d4227e4
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-23 12:36:36 -0600

    Create ProductCard component with proptypes and default values

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,34 @@
+import "../styles/ProductCard.css";
+import PropTypes from "prop-types";
+
+class ProductCard extends React.Component {
+	//name (string), price (number), imageUrl (string)
+	//use default values for this properties
+	static propTypes = {
+		name: PropTypes.string.isRequired,
+		price: PropTypes.number.isRequired,
+		imageUrl: PropTypes.string.isRequired,
+	};
+
+	static defaultProps = {
+		name: "Product Name",
+		price: 0.0,
+		imageUrl: "https://via.placeholder.com/150",
+	};
+
+	render() {
+		const name = this.props.name;
+		const price = this.props.price;
+		const imageUrl = this.props.imageUrl;
+
+		return (
+			<div>
+				<img src={imageUrl} alt={name} />
+				<h3>{name}</h3>
+				<p>{price}</p>
+			</div>
+		);
+	}
+}
+
+export default ProductCard;
